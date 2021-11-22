7bd4128cd0d0e79388e334a1511ec13d51c5164e
Yael Sprikut <yael@yaelmbpro.local>
2021-11-22 15:10:19 -0500

    feature progress

---
+++ src/TopComment.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Dropdown from 'react-bootstrap/Dropdown'
+// import './TopComment.scss'
+import { subreddits } from './api/client'
+
+interface Props {
+  topComment: any
+  topCommentAuth: any
+}
+
+interface State {}
+
+class TopComment extends React.Component<Props, State> {
+  render() {
+    const { topComment, topCommentAuth } = this.props
+    return (
+      <Card>
+        <Card.Header>{topCommentAuth}</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            {topComment}
+          </blockquote>
+        </Card.Body>
+      </Card>
+    )
+  }
+}
+
+export default TopComment
